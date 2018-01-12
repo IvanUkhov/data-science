d0a960b3706c6b22934c5bc9e786900c3629077b
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-01-12 09:50:33 +0100

    Adult: Move the auxiliary modules to a common location

--- package/facets/__init__.py
+++ package/facets/__init__.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-sys.path.append(os.path.join('facets/facets_overview/python'))
+sys.path.append(os.path.join(os.path.dirname(__file__),
+                             'source/facets_overview/python'))
 
 import base64
 
