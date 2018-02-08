5744fb4145b341e020346206ee6782bee03e692f
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-02-08 13:07:14 +0100

    MovieLens: Inspect the most popular movies

--- problem/movielens/data.py
+++ problem/movielens/data.py
@@ -1,25 +1,37 @@
+import numpy as np
 import pandas as pd
 
 
 class Data:
-    def value_counts(self):
-        return {column: self.data[column].nunique() for column in self.data.columns}
+    def describe(self):
+        def unique(series):
+            return series.nunique()
+
+        def missing(series):
+            return series.isnull().sum()
+
+        def zero(series):
+            return len(series) - np.count_nonzero(series)
+
+        aggregates = self.data.aggregate([unique, missing, zero])
+        return self.data.describe().append(aggregates)
 
 
 class Movie(Data):
-    def load(path='data/movies.csv'):
-        return Movie(pd.read_csv(path))
+    def load(path='data/movies.csv', **arguments):
+        return Movie(pd.read_csv(path, **arguments))
 
     def __init__(self, data):
+        data.set_index('movieId', inplace=True)
         self.data = data
 
-    def describe(self, ids, column='title'):
-        return self.data.loc[ids][column].values.tolist()
+    def find(self, ids):
+        return self.data.loc[ids]
 
 
 class Rating(Data):
-    def load(path='data/ratings.csv'):
-        data = pd.read_csv(path)
+    def load(path='data/ratings.csv', **arguments):
+        data = pd.read_csv(path, **arguments)
         data['timestamp'] = pd.to_datetime(data['timestamp'], unit='s')
         return Rating(data)
 
