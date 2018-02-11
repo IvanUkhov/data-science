5f572794454a4354e479ad59993370ef66948c73
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-02-11 10:38:25 +0100

    MovieLens: Remove batch recommendations

--- problem/movielens/recommender.py
+++ problem/movielens/recommender.py
@@ -12,6 +12,9 @@ class Baseline:
         self.options = options
 
     def fit(self, data):
+        n_epoch = self.options.get('n_epoch', 10)
+        l_source = self.options.get('l_source', 15)
+        l_target = self.options.get('l_target', 15)
         encoded_sources = self.source_encoder.fit_transform(data[0])
         encoded_targets = self.target_encoder.fit_transform(data[1])
         data = (encoded_sources, encoded_targets, data[2])
@@ -19,14 +22,22 @@ class Baseline:
         self.n_target = len(self.target_encoder.classes_)
         self.rating = Rating(data, shape=(self.n_source, self.n_target))
         self.mean, self.source_biases, self.target_biases = \
-            Baseline._compute(self.rating.data, **self.options)
+            Baseline._compute(self.rating.data, n_epoch, l_source, l_target)
         return self
 
+    def predict(self, source, target):
+        source, target = self._encode(source, target)
+        return self.mean + \
+               self.source_biases[source] + \
+               self.target_biases[target]
+
+    def _encode(self, source=None, target=None):
+        if source: source = self.source_encoder.transform([source])[0]
+        if target: target = self.target_encoder.transform([target])[0]
+        return source, target
+
     @staticmethod
-    def _compute(data, **options):
-        n_epoch = options.get('n_epoch', 10)
-        l_source = options.get('l_source', 15)
-        l_target = options.get('l_target', 15)
+    def _compute(data, n_epoch, l_source, l_target):
         n_source, n_target = data.shape
         index = data.nonzero()
         mean = data[index].mean()
@@ -44,13 +55,6 @@ class Baseline:
                 l_source + source_counts)
         return mean, source_biases, target_biases
 
-    def predict(self, source, target):
-        source = self.source_encoder.transform([source])[0]
-        target = self.target_encoder.transform([target])[0]
-        return self.mean + \
-               self.source_biases[source] + \
-               self.target_biases[target]
-
 
 class NearestNeighbor(Baseline):
     def __init__(self, **options):
@@ -61,32 +65,28 @@ class NearestNeighbor(Baseline):
         self.similarity = Similarity(self.rating.data, **self.options)
         return self
 
-    def recommend(self, source, n_target=10, n_neighbor=10):
-        source = self.source_encoder.transform([source])[0]
-        ratings = self._predict_ratings(source, n_neighbor)
-        targets = _choose(n_target, np.argsort(-ratings, axis=1),
-                          lambda target: not self.rating[source, target])
-        return self.target_encoder.inverse_transform(targets), \
-               ratings[0, targets].tolist()[0]
-
-    def recommend_neighbors(self, source, n_neighbor=10):
-        source = self.source_encoder.transform([source])[0]
-        neighbors, similarities = self._find_neighbors(source, n_neighbor)
-        return self.source_encoder.inverse_transform(neighbors), \
-               similarities.tolist()[0]
-
-    def _find_neighbors(self, source, n_neighbor):
+    def connect(self, source, n_neighbor=10):
+        source, _ = self._encode(source=source)
         similarities = self.similarity[source, :].todense()
-        neighbors = _choose(n_neighbor, np.argsort(-similarities, axis=1),
-                            lambda neighbor: source != neighbor)
-        return neighbors, similarities[0, neighbors]
+        neighbors = _choose(
+            n_neighbor, np.argsort(-similarities, axis=1),
+            lambda neighbor: source != neighbor)
+        similarities = np.squeeze(np.asarray(similarities[0, neighbors]))
+        neighbors = self.source_encoder.inverse_transform(neighbors)
+        return neighbors, similarities
 
-    def _predict_ratings(self, source, n_neighbor):
-        neighbors, _ = self._find_neighbors(source, n_neighbor)
+    def predict(self, source, target, n_neighbor=10, n_neighbor_min=1):
+        source, target = self._encode(source=source, target=target)
+        similarities = self.similarity[source, :].todense()
+        neighbors = _choose(
+            n_neighbor, np.argsort(-similarities, axis=1),
+            lambda neighbor: source != neighbor and
+                             self.rating[neighbor, target])
+        if len(neighbors) < n_neighbor_min:
+            return super(NearestNeighbor, self).predict(source, target)
+        ratings = self.rating[neighbors, target]
         similarities = self.similarity[source, neighbors]
-        ratings = self.rating[neighbors, :]
-        return np.divide(similarities.dot(ratings),
-                         np.abs(similarities).dot(ratings > 0))
+        return similarities.dot(ratings)[0, 0] / np.sum(np.abs(similarities))
 
 
 class Matrix:
