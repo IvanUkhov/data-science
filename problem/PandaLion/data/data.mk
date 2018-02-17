fc65af14f05b39828c2f810bad1a1728c0d99b59
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-02-17 19:45:45 +0100

    PandaLion: Preprocess the images

--- problem/PandaLion/data/data.mk
+++ problem/PandaLion/data/data.mk
@@ -4,21 +4,45 @@ wnid := $(2)
 
 imagenet_url := http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=$${wnid}
 
-images: list.txt
-	mkdir -p $$@
-	-cd $$@ && \
-		cat ../$$< | \
-		grep -v \' | \
-		grep -v '^$$$$' | \
-		sed $$$$'s/\r//' | \
-		xargs -n1 -P8 -t \
-		curl --silent --connect-timeout 10 -LO
+all: .renamed
 
 list.txt:
 	curl -L $${imagenet_url} -o $$@
 
-clean:
-	rm -rf images
+.downloaded: list.txt
+	mkdir -p images
+	-cd images && \
+		cat ../$$< | \
+		sed $$$$'s/\r//' | \
+		grep -i '\.\(jpg\|jpeg\)$$$$' | \
+		grep -v \' | \
+		xargs -n1 -P8 -t \
+		curl --connect-timeout 10 --fail --silent -LO
+	touch $$@
 
-.PHONY: clean
+.cleaned: .downloaded
+	cd images; \
+	shopt -s nocaseglob; \
+	for name in *.{jpg,jpeg}; do \
+		if [[ ! $$$$(file -b "$$$${name}") =~ JPEG ]]; then \
+			rm "$$$${name}"; \
+		fi; \
+	done
+	touch $$@
+
+.renamed: .cleaned
+	cd images; \
+	number=1; \
+	shopt -s nocaseglob; \
+	for old in *.{jpg,jpeg}; do \
+		new=$$$$(printf "%04d.jpg" "$$$${number}"); \
+		mv "$$$${old}" "$$$${new}"; \
+		let number=number+1; \
+	done
+	touch $$@
+
+clean:
+	rm -rf .downloaded .renamed images
+
+.PHONY: all clean
 endef
