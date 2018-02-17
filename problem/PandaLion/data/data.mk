df97f4286f7ac7a66d9f9d777014b0567cc44ae2
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-02-17 16:42:08 +0100

    PandaLion: Download the images

--- problem/PandaLion/data/data.mk
+++ problem/PandaLion/data/data.mk
@@ -4,6 +4,21 @@ wnid := $(2)
 
 imagenet_url := http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=$${wnid}
 
+images: list.txt
+	mkdir -p $$@
+	-cd $$@ && \
+		cat ../$$< | \
+		grep -v \' | \
+		grep -v '^$$$$' | \
+		sed $$$$'s/\r//' | \
+		xargs -n1 -P8 -t \
+		curl --silent --connect-timeout 10 -LO
+
 list.txt:
 	curl -L $${imagenet_url} -o $$@
+
+clean:
+	rm -rf images
+
+.PHONY: clean
 endef
