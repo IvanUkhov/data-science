d0316748967818179214fde3eaa8de31c0d139bf
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-02-20 13:32:55 +0100

    LifeIndex: Rename LS to LI

--- problem/LifeIndex/analysis.R
+++ problem/LifeIndex/analysis.R
@@ -8,7 +8,7 @@ load.BLI <- function(path) {
   data <- subset(data, Indicator == 'Life satisfaction')
   data <- data[c('Country', 'Value')]
   data <- subset(data, Country != 'OECD - Total')
-  names(data)[2] <- 'LS'
+  names(data)[2] <- 'LI'
   return(data)
 }
 
@@ -34,10 +34,10 @@ load <- function(path.BLI, path.IMF) {
 plot.scatter <- function(data) {
   n <- nlevels(data$Country)
   shapes <- rep(seq(1, 6), ceiling(n / 6))[1:n]
-  plot <- ggplot(data, aes(x = GDP, y = LS, color = Country, shape = Country))
+  plot <- ggplot(data, aes(x = GDP, y = LI, color = Country, shape = Country))
   plot <- plot + scale_shape_manual(values = shapes)
   plot <- plot + geom_point(size = 2, stroke = 1)
-  plot <- plot + xlab('GDP per capita') + ylab('Life satisfaction')
+  plot <- plot + xlab('GDP per capita') + ylab('Life index')
   return(plot)
 }
 
