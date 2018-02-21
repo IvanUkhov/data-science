27d69a10e9299265822cdf89dd0f57f2b0193138
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-02-21 15:46:31 +0100

    LifeIndex: Make a cosmetic adjustment

--- problem/LifeIndex/analysis.R
+++ problem/LifeIndex/analysis.R
@@ -1,9 +1,10 @@
 plot.scatter <- function(data) {
   n <- nlevels(data$Country)
   shapes <- rep(seq(1, 6), ceiling(n / 6))[1:n]
-  plot <- ggplot(data, aes(x = GDP, y = LI, color = Country, shape = Country))
-  plot <- plot + scale_shape_manual(values = shapes)
-  plot <- plot + geom_point(size = 2, stroke = 1)
-  plot <- plot + xlab('GDP per capita') + ylab('Life index')
+  plot <- ggplot(data, aes(x = GDP, y = LI, color = Country, shape = Country)) +
+    scale_shape_manual(values = shapes) +
+    geom_point(size = 2, stroke = 1) +
+    xlab('GDP per capita') +
+    ylab('Life index')
   return(plot)
 }
