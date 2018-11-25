99711d6e3813a75630e0b195bea47617c862579d
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-11-25 21:57:02 +0100

    BayesianAB: Refactor the plotting function

--- problem/BayesianAB/plotting.R
+++ problem/BayesianAB/plotting.R
@@ -1,13 +1,20 @@
 plot <- function(data,
-                 line = 0.05,
-                 color = 'pass_end',
-                 y_aestetic = 'p_value',
+                 y = 'expected_loss',
+                 color = 'passed_any',
+                 line = NA,
                  labels = c('Not passed', 'Passed')) {
+  day_max <- max(data$day)
+  end_points <- data %>%
+    group_by(replication) %>%
+    filter(day == max(day),
+           day != day_max) %>%
+    slice(1)
   data %>%
-    ggplot(aes_string('day', y_aestetic, group = 'replication')) +
+    ggplot(aes_string('day', y, group = 'replication')) +
     geom_line(aes_string(alpha = color, color = color)) +
-    geom_hline(color = 'red', yintercept = line, lty = 2) +
-    scale_color_manual(values = c('black', 'red'), labels = labels) +
-    scale_alpha_manual(values = c(0.15, 1), labels = labels) +
+    geom_hline(color = 'orange', yintercept = line, lty = 2) +
+    geom_point(data = end_points, color = 'orange') +
+    scale_color_manual(values = c('black', 'orange'), labels = labels) +
+    scale_alpha_manual(values = c(0.1, 1), labels = labels) +
     labs(x = 'Day of experiment', color = '', alpha = '')
 }
